5287a4c174dcd426b635003a61ba94c95e2ff05e
Einsam <snitxmhm@gmail.com>
2022-12-31 16:52:45 +0800

    initial project

---
+++ src/main/java/com/hanpi/hanpicraft/Hanpicraft.java
@@ -0,0 +1,5 @@
+package com.hanpi.hanpicraft;
+import net.minecraftforge.fml.common.Mod;
+@Mod(Utils.MOD_ID)
+public class Hanpicraft {
+}
---
+++ src/main/java/com/hanpi/hanpicraft/Utils.java
@@ -0,0 +1,5 @@
+package com.hanpi.hanpicraft;
+
+public class Utils {
+    public static final String MOD_ID = "hanpicraft";
+}
